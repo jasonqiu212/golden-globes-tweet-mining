149eb92d842df3199f1788bb4ec26d8072cd788e
jasonqiu212 <jason_qiu@hotmail.com>
2023-10-20 20:40:46 -0500

    Add methods removeExtraWhitespace and removeURLs

--- preprocess.py
+++ preprocess.py
@@ -2,30 +2,32 @@
 
 import ftfy
 from langdetect import detect
-import unidecode
+import re
+from unidecode import unidecode
 
 def fixMojibake(str):
+    """
+    Fixes mojibake in text.
+    """
     return ftfy.fix_text(str)
 
 def transformToASCII(str):
+    """
+    Replaces non-ASCII characters with ASCII counterparts.
+    Removes characters otherwise.
+    """
     return unidecode(str)
 
-def removeURLs(str):
-    return str
+def removeExtraWhitespace(str):
+    """Replaces extra whitespace with a single space."""
+    return re.sub(" +", " ", str)
 
-def removeWhitespace(str):
-    return str
+def removeURLs(str):
+    """Removes URLs from text."""
+    return re.sub("https?:\/\/(\S*)", "", str)
 
 def isEnglish(str):
-    """
-    Checks if text is in English.
-
-    Args:
-        str: Text to check.
-    
-    Returns:
-        True, if text is in English. False otherwise.
-    """
+    """Checks if text is in English."""
     return detect(str) == 'en'
 
 def preprocess(tweets):
@@ -39,8 +41,8 @@ def preprocess(tweets):
         Preprocessed list of tweets.
     """
     for tweet in tweets:
-        tweet.applyPreprocessing(fixMojibake)
-        tweet.applyPreprocessing(transformToASCII)
-        tweet.applyPreprocessing(removeURLs)
-        tweet.applyPreprocessing(removeWhitespace)
-    return filter(lambda t : isEnglish(t.text), tweets)
+        tweet.applyTextPreprocessing(fixMojibake)
+        tweet.applyTextPreprocessing(transformToASCII)
+        tweet.applyTextPreprocessing(removeExtraWhitespace)
+        tweet.applyTextPreprocessing(removeURLs)
+    return list(filter(lambda t : isEnglish(t.text), tweets))
