167ca4162305bef0c1138683b07184519308eb86
jasonqiu212 <jason_qiu@hotmail.com>
2023-10-25 23:52:00 -0400

    Add methods to write and read result file

---
+++ file.py
@@ -0,0 +1,65 @@
+"""This file provides operations to read and write to files."""
+
+import json
+
+from tweet import Tweet
+
+
+def load_tweets(year=2013):
+    """
+    Loads tweets from JSON data file.
+
+    Args:
+        year: The year of the award ceremony.
+    Returns:
+        List of tweets from JSON file.
+    """
+    # with open("gg{}.json".format(year)) as json_file:
+    with open("gg{}-subset.json".format(year)) as json_file:
+        data = json.load(json_file)
+
+        return list(map(lambda t: Tweet(t['text'], t['user']['screen_name']), data))
+
+
+def load_awards(year=2013):
+    """
+    Loads official award names from JSON answer file.
+
+    Args:
+        year: The year of the award ceremony.
+    Returns:
+        List of official award names from JSON file.
+    """
+    with open("gg{}answers.json".format(year)) as json_file:
+        data = json.load(json_file)
+        return list(data['award_data'].keys())
+
+
+def write_results(data):
+    """Writes extracted information to JSON results file."""
+    return
+
+
+def write_human_readable_results(data):
+    """Writes extracted information to human-readable results file."""
+    return
+
+
+def read_results(year, component):
+    """
+    Reads specified component from JSON results file.
+
+    Args:
+        year: The year of the award ceremony.
+        component: Component of results to read.
+    Returns:
+        List of host or award names, if for 'hosts' or 'awards' components.
+        Dictionary with hard-coded award names as keys and results as entries, 
+        if for 'nominees', 'presenters', or 'winner' components.
+    """
+    with open("gg{}results.json".format(year)) as json_file:
+        data = json.load(json_file)
+        if component == 'hosts' or component == 'awards':
+            return data[component]
+        awards = list(data['award_results'].keys())
+        return dict((award, data['award_results'][award][component]) for award in awards)
--- gg_api.py
+++ gg_api.py
@@ -1,70 +1,38 @@
-from load_json import load_json
+import file
 from preprocess import preprocess
-
-import spacy
-nlp = spacy.load("en_core_web_sm")
-
-
-def find_relevant_tweets(tweets, keywords):
-    # 过滤出包含关键词的推文, 关键词（win，won，got，etc,在main中定义keywords）
-    relevant_tweets = []
-    for tweet in tweets:
-        if any(keyword.lower() in tweet["text"].lower() for keyword in keywords):
-            relevant_tweets.append(tweet)
-    return relevant_tweets
-# keywords = ["award", "winner", "won", "receives", "recipient", "nominee", "nominated", "prize", "congratulations", "honored"]
-
-
-def extract_award_info(tweet):
-    # 使用spaCy进行实体识别和依存关系解析，这个是我google出来的方法，不知道是否允许使用，也是jmy之前提到的快速查询出句子中有用的entities的方法。
-    doc = nlp(tweet["text"])
-    entities = [(ent.text, ent.label_) for ent in doc.ents]
-    # 查找可能的获奖者和奖项
-    persons = [ent for ent in entities if ent[1] == 'PERSON']
-    awards = [ent for ent in entities if ent[1]
-              == 'WORK_OF_ART' or ent[1] == 'EVENT']
-
-    # 这里可以添加更多的逻辑来验证和精炼获奖者和奖项信息
-
-    return persons, awards
 
 
 def get_hosts(year):
     '''Hosts is a list of one or more strings. Do NOT change the name
     of this function or what it returns.'''
-    # Your code here
-    return hosts
+    return file.read_results(year, 'hosts')
 
 
 def get_awards(year):
     '''Awards is a list of strings. Do NOT change the name
     of this function or what it returns.'''
-    # Your code here
-    return awards
+    return file.read_results(year, 'awards')
 
 
 def get_nominees(year):
     '''Nominees is a dictionary with the hard coded award
     names as keys, and each entry a list of strings. Do NOT change
     the name of this function or what it returns.'''
-    # Your code here
-    return nominees
+    return file.read_results(year, 'awards')
 
 
 def get_winner(year):
     '''Winners is a dictionary with the hard coded award
     names as keys, and each entry containing a single string.
     Do NOT change the name of this function or what it returns.'''
-    # Your code here
-    return winners
+    return file.read_results(year, 'winner')
 
 
 def get_presenters(year):
     '''Presenters is a dictionary with the hard coded award
     names as keys, and each entry a list of strings. Do NOT change the
     name of this function or what it returns.'''
-    # Your code here
-    return presenters
+    return file.read_results(year, 'presenters')
 
 
 def pre_ceremony():
@@ -72,7 +40,6 @@ def pre_ceremony():
     will use, and stores that data in your DB or in a json, csv, or
     plain text file. It is the first thing the TA will run when grading.
     Do NOT change the name of this function or what it returns.'''
-    # Your code here
     print("Pre-ceremony processing complete.")
     return
 
@@ -83,20 +50,10 @@ def main():
     and then run gg_api.main(). This is the second thing the TA will
     run when grading. Do NOT change the name of this function or
     what it returns.'''
-    tweets = load_json(2013)
+    tweets = file.load_tweets(2013)
     print(preprocess(tweets))
-
-    # 新添加的关键词查找法
-    keywords = ["award", "winner", "won", "receives", "recipient",
-                "nominee", "nominated", "prize", "congratulations", "honored", "got"]
-    relevant_tweets = find_relevant_tweets(tweets, keywords)
-
-    for tweet in relevant_tweets:
-        persons, awards = extract_award_info(tweet)
-        print(f"Tweet: {tweet['text']}")
-        print(f"Possible winners: {persons}")
-        print(f"Possible awards: {awards}")
-    return
+    # perform the extraction
+    #
 
 
 if __name__ == '__main__':
