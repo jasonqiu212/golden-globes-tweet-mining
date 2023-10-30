ac2a16979d783ae6029f2e093b8a19b0240972d7
jasonqiu212 <jason_qiu@hotmail.com>
2023-10-30 00:30:16 -0400

    Fix preprocess of @ and #

--- preprocess.py
+++ preprocess.py
@@ -5,53 +5,67 @@ from langdetect import detect
 import re
 from unidecode import unidecode
 
-def fixMojibake(str):
+
+def fix_mojibake(str):
     """
     Fixes mojibake in text.
     """
     return ftfy.fix_text(str)
 
-def transformToASCII(str):
+
+def transform_to_ASCII(str):
     """
     Replaces non-ASCII characters with ASCII counterparts.
     Removes characters otherwise.
     """
     return unidecode(str)
 
-def removeExtraWhitespace(str):
+
+def remove_extra_whitespace(str):
     """Replaces extra whitespace with a single space."""
     return re.sub(" +", " ", str)
 
-def removeURLs(str):
+
+def remove_URLs(str):
     """Removes URLs from text."""
     return re.sub("https?:\/\/(\S*)", "", str)
 
-def isEnglish(str):
+
+def is_english(str):
     """Checks if text is in English."""
     return detect(str) == 'en'
 
-def removeAT(str):
-    text_without_at = str.replace("@", "")
-    return text_without_at
 
-def gethash(str):
+def remove_symbol_and_split(match):
+    return re.sub(r'([a-z])([A-Z])', r'\1 \2', match.group(1))
+
+
+def process_ats(str):
+    return re.sub("@(\S*)", remove_symbol_and_split, str)
+
+
+def process_hashtags(str):
+    return re.sub("#(\S*)", remove_symbol_and_split, str)
+
+
+def get_hashtags(str):
     hashtags = re.findall(r'#\w+', str)
     list = []
     for i in hashtags:
         word_without_hashtag = i.lstrip('#')
-        formatted_word = re.sub(r'([a-z])([A-Z])', r'\1 \2', word_without_hashtag)
+        formatted_word = re.sub(
+            r'([a-z])([A-Z])', r'\1 \2', word_without_hashtag)
         list.append(formatted_word)
     return list
 
 
-
 def preprocess(tweets):
-
     for tweet in tweets:
-        tweet.applyTextPreprocessing(fixMojibake)
-        tweet.applyTextPreprocessing(transformToASCII)
-        tweet.applyTextPreprocessing(removeExtraWhitespace)
-        tweet.applyTextPreprocessing(removeURLs)
-        tweet.applyTextPreprocessing(removeAT)
-        tweet.getHashTag(gethash)
-    return list(filter(lambda t : isEnglish(t.text), tweets))
+        tweet.applyTextPreprocessing(fix_mojibake)
+        tweet.applyTextPreprocessing(transform_to_ASCII)
+        tweet.applyTextPreprocessing(remove_extra_whitespace)
+        tweet.applyTextPreprocessing(remove_URLs)
+        tweet.applyTextPreprocessing(process_ats)
+        tweet.hashtags = get_hashtags(tweet.text)
+        tweet.applyTextPreprocessing(process_hashtags)
+    return list(filter(lambda t: is_english(t.text), tweets))
--- tweet.py
+++ tweet.py
@@ -4,15 +4,16 @@ class Tweet:
     def __init__(self, text, user):
         self.text = text
         self.user = user
-        self.hashtag = []
-    
-    def applyTextPreprocessing(self, f):
+        self.hashtags = []
 
+    def applyTextPreprocessing(self, f):
+        """
+        Apply text processing function onto the text of this tweet.
+
+        Args:
+            f: Text processing function to apply.
+        """
         self.text = f(self.text)
 
-    def getHashTag(self, g):
-
-        self.hashtag = g(self.text)
-    
     def __repr__(self) -> str:
-        return "(User: {0}, Text: {1}, Hashtag: {2})".format(self.user, self.text, self.hashtag)
+        return "(User: {0}, Text: {1}, Hashtags: {2})".format(self.user, self.text, self.hashtags)
