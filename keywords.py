3d5cab7d2f75fc326a43dc734df7b724b83e72d9
jasonqiu212 <jason_qiu@hotmail.com>
2023-10-31 10:38:32 -0500

    Add award keywords

--- keywords.py
+++ keywords.py
@@ -1,5 +1,10 @@
 """This file contains constants of keywords for parsing."""
 
+AWARD_CATEGORIES = ['film', 'motion picture', 'television', 'series',
+                    'song', 'score', 'actress', 'actor', 'director', 'screenplay', 'script']
+AWARD_QUALIFIERS = ['motion picture', 'television', 'mini-series', 'series',
+                    'comedy', 'musical', 'drama', 'animated', 'supporting', 'foreign language']
+
 WINNERS = ["award", "winner", "won", "receives", "recipient",
            "nominee", "nominated", "prize", "congratulations", "honored", "got"]
 
