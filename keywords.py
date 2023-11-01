e3522049791c84864962e5ae276bda3a9f0b6f7a
jasonqiu212 <jason_qiu@hotmail.com>
2023-10-31 22:38:50 -0500

    Adjust award keywords

--- keywords.py
+++ keywords.py
@@ -1,9 +1,9 @@
 """This file contains constants of keywords for parsing."""
 
-AWARD_CATEGORIES = ['film', 'motion picture', 'television', 'series',
-                    'song', 'score', 'actress', 'actor', 'director', 'screenplay', 'script']
-AWARD_QUALIFIERS = ['motion picture', 'television', 'mini-series', 'series',
-                    'comedy', 'musical', 'drama', 'animated', 'supporting', 'foreign language']
+AWARD_CATEGORIES = {'film', 'picture', 'television', 'series',
+                    'song', 'score', 'actress', 'actor', 'director', 'screenplay', 'script'}
+AWARD_QUALIFIERS = {'picture', 'television', 'mini-series', 'series',
+                    'comedy', 'musical', 'drama', 'animated', 'supporting', 'foreign', 'language'}
 
 WINNERS = ["award", "winner", "won", "receives", "recipient",
            "nominee", "nominated", "prize", "congratulations", "honored", "got"]
